be1382a2c615e1098ce58d65bee05acab3d94898
Ali Elsokary <ali.elsokary8@gmail.com>
2020-06-28 16:02:46 +0200

    "I got a delivery for you." Create Xcode project.

---
+++ ThreatLevelMidnight/ThreatLevelMidnight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThreatLevelMidnight
+//
+//  Created by Ali Elsokary on 6/28/20.
+//  Copyright © 2020 mag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
